8071ae2f12d696a6582d875610d52fb5a1c63618
BrianCoveney <coveneybrian@gmail.com>
2016-07-19 09:41:03 +0100

    scroll to top button added

--- src/js/myjquery.js
+++ src/js/myjquery.js
@@ -5,6 +5,30 @@ $(document).ready(function() {
 				}
 				$('html, body').animate({
 					scrollTop: movePos
-				}, 1000);
+				}, 800);
 			});
-		});
+
+    var scroll_top_duration = 700;
+
+    /*** Smooth Scroll to Top ***/
+    $('.scrollTop').click(function (event) {
+        event.preventDefault();
+        $('body,html').animate({
+                scrollTop: 0,
+            }, scroll_top_duration
+        );
+    });
+
+
+    /*** Floating scroll button ***/
+    var amountScrolled = 300;
+    var duration = 500;
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() != 0) {
+            $('.scrollTopWrapper').fadeIn(duration);
+        } else {
+            $('.scrollTopWrapper').fadeOut(duration);
+        }
+    });
+});
