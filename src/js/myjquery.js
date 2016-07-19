8a30ca8c851c0203a34e345c04b03b51c7d3ddee
BrianCoveney <coveneybrian@gmail.com>
2016-07-19 14:29:14 +0100

    added smooth scrolling to anchor links on same page

--- src/js/myjquery.js
+++ src/js/myjquery.js
@@ -31,4 +31,28 @@ $(document).ready(function() {
             $('.scrollTopWrapper').fadeOut(duration);
         }
     });
+
+
+
+    //*** Smooth scroling to anchor on the same page ***/
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+
+
+    $('a:link').click(function()
+    {
+        $('body a').removeClass('active');
+        $(this).addClass('active');
+    });
+
 });
