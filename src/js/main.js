3e28712900882cef595128b2c51d125ce8476656
BrianCoveney <coveneybrian@gmail.com>
2016-07-24 17:34:50 +0100

    layout tinkering - circle with text wrapping around it, via the shape-outside property. Add a Polygon layout via transform:skew(..deg) property, to the second vp

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,14 @@
 var s, z, f;
 
-s = document.getElementsByClassName('content');
-s[0].innerHTML = " < SECTION > ";
+// s = document.getElementsByClassName('content');
+// s[0].innerHTML =
+// 	"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 
-z = document.getElementsByClassName('content2');
-for(var i = 0; i < z.length; i++){
-	z[i].innerHTML = " < TESTING > ";
-}
+
+// z = document.getElementsByClassName('content2');
+// for(var i = 0; i < z.length; i++){
+// 	z[i].innerHTML = " < TESTING > ";
+// }
 
 f = document.getElementById('mFooter').
 	getElementsByClassName('footer_cont')[0];
